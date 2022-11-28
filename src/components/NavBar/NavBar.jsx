863eff97567ff25e4927b57e3b3fbdc891a7f8cb
Franco Parodi <francoparodi2001@gmail.com>
2022-11-28 14:43:54 -0300

    SegundaEntregaFrancoParodi

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,7 +7,8 @@ import { AiOutlineUser} from "react-icons/ai";
 import CartWidget from '../CartWidget/CartWidget';
 import { AiFillShopping } from "react-icons/ai";
 import NigthMode from '../NightMode/NigthMode';
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
+import {Link} from 'react-router-dom';
 import '../.././Style.css';
 
 
@@ -33,10 +34,10 @@ const NavBar = () => {
                 <Nav.Link id='textoNavBar' href="#home">Home</Nav.Link>
                 <Nav.Link id='textoNavBar' href="#link">About us</Nav.Link>
                   <NavDropdown title="Category" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="/category/jewelery">Jewelery</NavDropdown.Item>
-                    <NavDropdown.Item href="/category/electronics">Electronics</NavDropdown.Item>
-                    <NavDropdown.Item href="/category/men's clothing">Men's clothing</NavDropdown.Item>
-                    <NavDropdown.Item href="/category/women's clothing">Women's clothing</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to="/category/jewelery">Jewelery</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to="/category/electronics">Electronics</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to="/category/men's clothing">Men's clothing</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to="/category/women's clothing">Women's clothing</NavDropdown.Item>
                   </NavDropdown>
                   <NigthMode handleColor={onChangeColor}/>
               </Nav>
--- src/components/NightMode/NigthMode.jsx
+++ src/components/NightMode/NigthMode.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const NigthMode = ({handleColor}) => {
   return (
     <select name="navColor" id="navColor" onChange={handleColor}>
-        <option value="8b77fd">Purple</option>
+        <option value="#8b77fd">Purple</option>
         <option value="#fd77fd">Pink</option>
         <option value="#f5f5f5">white</option>
     </select>
